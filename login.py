1a7481f6e57b220c53b20f6e2c900f20f2f16b14
gelman_vai <gelman2212@gmail.com>
2023-06-25 01:07:48 +0600

    I am login.py file.login.py file has been committed to .git local repo.

---
+++ login.py
@@ -0,0 +1,8 @@
+I am login file.
+My task is is to do all the fnctions related to user login.
+
+
+login.py file has been added to .git repo(local git repo).
+login.py file has been added to .git repo(local git repo) for second time.
+login.py file has been added to .git repo(local git repo)for third time.
+login.py file has been added to .git repo(local git repo) for fourth time.
